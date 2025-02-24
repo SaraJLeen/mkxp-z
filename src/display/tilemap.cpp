e39e8482f8899f0477b6c3c620b8c61dfd3615e2
Sara Leen <sara@wingdreams.net>
2025-02-24 02:19:09 -0500

    Fix speed of autotiles when not running at 40 FPS

--- src/display/tilemap.cpp
+++ src/display/tilemap.cpp
@@ -888,7 +888,7 @@ struct TilemapPrivate
 			tilemapShader.setTone(tone->norm);
 			tilemapShader.setColor(color->norm);
 			tilemapShader.setOpacity(opacity.norm);
-			tilemapShader.setAniIndex(tiles.aniIdx / atFrameDur);
+			tilemapShader.setAniIndex(tiles.aniIdx / (atFrameDur*(shState->graphics().getFrameRate()/40.f)));
 			tilemapShader.setATFrames(atlas.nATFrames);
 			shaderVar = &tilemapShader;
 		}
